88a20df26d314f4fdad6978ae9ee65b6b59fef24
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-10-07 20:15:46 +1100

    docs: add some notes on successful install

--- examples/dreambooth/NOTES.md
+++ examples/dreambooth/NOTES.md
@@ -10,3 +10,23 @@ accelerate config
 #What GPU(s) (by id) should be used for training on this machine as a comma-seperated list? [all]:
 #Do you wish to use FP16 or BF16 (mixed precision)? [NO/fp16/bf16]: 
 ```
+
+# Setup pytorch for CUDA11
+
+* https://discuss.pytorch.org/t/nvidia-geforce-rtx-3090-with-cuda-capability-sm-86-is-not-compatible-with-the-current-pytorch-installation/141940/23
+* https://discuss.pytorch.org/t/need-help-trouble-with-cuda-capability-sm-86/120235/39
+    ```
+    pippip uninstall torch torchvision 
+    pip3 install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+    ```
+* Debug 
+    ```
+    import torch
+
+    torch.cuda.get_arch_list()
+    torch.version.cuda
+    ```
+
+# Reducing memory
+
+* https://github.com/ShivamShrirao/diffusers/tree/main/examples/dreambooth
---
+++ examples/dreambooth/init.sh
@@ -0,0 +1,35 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="/workspace/examples/dreambooth/data"
+export CLASS_DIR="/workspace/examples/dreambooth/class"
+export OUTPUT_DIR="/workspace/examples/dreambooth/output"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of sks dog" \
+  --class_prompt="a photo of dog" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 --gradient_checkpointing \
+  --use_8bit_adam \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --max_train_steps=800
+
+# accelerate launch train_dreambooth.py \
+#   --pretrained_model_name_or_path=$MODEL_NAME  \
+#   --instance_data_dir=$INSTANCE_DIR \
+#   --output_dir=$OUTPUT_DIR \
+#   --instance_prompt="a photo of sks dog" \
+#   --resolution=512 \
+#   --train_batch_size=1 \
+#   --gradient_accumulation_steps=1 \
+#   --learning_rate=5e-6 \
+#   --lr_scheduler="constant" \
+#   --lr_warmup_steps=0 \
+#   --max_train_steps=400
